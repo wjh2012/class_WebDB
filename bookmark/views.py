f16b965aec5abfd232d919b8e8eedeb14d85c61c
wjh <wjh2012@naver.com>
2020-09-17 17:03:14 +0900

    first

---
+++ bookmark/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
+# Create your views here.
+# ListView를 상속받은 클래스는 object_list 멤버변수를 사용할 수 있다
+class BookmarkLV(ListView):
+    model=Bookmark
+
+class BookmarkDV(DetailView):
+    model = Bookmark
